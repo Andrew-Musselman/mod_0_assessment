5c72c293d4c50cb6174541247b82c76a81eb9a76
Andrew Musselman <andrew.mussels@gmail.com>
2021-10-22 08:51:52 -0600

    Complete modelA.js

---
+++ modelA.js
@@ -0,0 +1,10 @@
+var airport = {
+  name: 'DIA',
+  location: 'Denver',
+  terminals: 1,
+  areaMilesSquared: 53,
+  isIlluminati: true,
+  isHaunted: true,
+  airlines: ['Delta', 'Frontier', 'American Airlines' , 'Southwest'],
+  concourses: ['Concourse A' , 'Concourse B' , 'Concourse C'],
+}
