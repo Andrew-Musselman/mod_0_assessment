8595c0c5d480d0468d8ec5ab2d2b80e7686805b6
Andrew Musselman <andrew.mussels@gmail.com>
2021-10-22 08:43:01 -0600

    Complete program speecific challenge

---
+++ variables.js
@@ -0,0 +1,9 @@
+var famousPerson = 'Beyonce';
+var isActive = true;
+famousPerson = 'Rihanna';
+var num3 = 10;
+var num9 = 27;
+var sum = num3 + num9;
+var animals = ['cats', 'dogs', 'kangaroos'];
+animals.shift();
+var len = famousPerson.length; 
